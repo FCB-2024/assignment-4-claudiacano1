00cf2575b4efdc95b6f8bb4cab6da22ec3ff5d1d
claudia cano <claudia.cano@estudiant.upf.edu>
2024-10-20 20:56:47 +0200

    First attempt

--- src/antiprime.py
+++ src/antiprime.py
@@ -5,13 +5,37 @@ def main() :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
 
+	i = int ("1")
+	r = int ("0")
+
+	while i <= x:
+		if x % i == 0:
+			r = r + int ("1")
+		i = i + int ("1")
+
+	s = int ("0")
+	p = x - int ("1")
+
+	while p >= int ("1") and s < r:
+		f = int ("1")
+		s = int ("0")
+
+		while f <= p:
+			if p % f == 0:
+				s = s + int ("1")
+			f = f + int ("1")
+		p = p - int ("1")
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
+
+	if s >= r:
+		return ("not anti-prime")
+	else: 
+		return ("anti-prime")
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
@@ -21,4 +45,5 @@ if __name__ == "__main__" :
 	## $ python antiprime.py 6
 	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
 	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
+	x = int (input("enter one value: "))
 	print(main())
